17b6995a96786ed2b7a82532e80aa8b8b88d68cd
evica-ai <e_mai@fanshaweonline.ca>
2023-01-31 15:27:31 -0500

    messaging proof

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const app = express();
+
 const http = require('http');
 const server = http.createServer(app);
 
-// if port does not exist, it will default to host 3000
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
 const port = process.env.PORT || 3000;
 
-// app.get is a route handler
-// reference in socket.io, get started serving html
+//tell express where to find static web files
+app.use(express.static('public'));
+
+//app.get is a route handler the / is the route
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
-
-// an example of a route eg localhost:3000/contact , localhost:3000/brodie
-app.get('/contact', (req, res) => {
-    res.sendFile(__dirname + '/contact.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
-
-
 
 server.listen(port, () => {
   console.log(`listening on ${port}`);
+});
+
+// spcket.io stuff 
+// whenever smeone connects, goes to socket
+io.on('connection', (socket) => {
+  console.log('a user connected', socket);
+  socket.emit('connected', {sID: socket.id, message: 'new connection'});
+
+
+  // liste for incimng message from anyone connected to the chat service and then see what the message is
+  socket.on('chat_message', function(msg) {
+    console.log(msg);
+
+    // step 2 - show everype what was sent though (send the message to everyone connected to the service)
+   io.emit('new_message', {message: msg})
+  })
 });
---
+++ public/js/main.js
@@ -0,0 +1,45 @@
+//import will always go at the top 
+import ChatMsg from './components/ChatMessage.js';
+
+const socket = io();
+
+// utility functions for socket
+function setUserID ({sID}) {
+    // debugger;
+    // save our unique ID generaed by Socket on the server side - this is how we track individual connects to the chat service
+    vm.socketID= sID;
+}
+
+function showNewMessage({message}) {
+    vm.messages.push(message);
+}
+const {createApp} = Vue
+
+const vm = createApp({
+    data() {
+        return {
+          socketID: '',
+          message: '',
+          messages: []
+        }
+    },
+
+    methods: {
+        dispatchMessage() {
+            socket.emit('chat_message', {
+                content: this.message,
+                name: this.nickname || 'anonymous'
+            })
+
+            this.message = "";
+        }
+    },
+
+    components: {
+        newmsg: ChatMsg
+    }
+
+}).mount('#app')
+
+socket.addEventListener('connected', setUserID);
+socket.addEventListener('new_message', showNewMessage);
